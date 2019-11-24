a48400361b85333ebf358c1614d6932e6d82af21
nickel <nicolas.wenzel@gmx.de>
2019-11-24 17:22:41 +0100

    Update example in test_package with code taken from https://arduinojson.org/v6/example/generator/

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -9,41 +9,30 @@
 #include <iostream>
 
 int main() {
-  // Memory pool for JSON object tree.
+  // Allocate the JSON document
   //
-  // Inside the brackets, 512 is the size of the pool in bytes.
-  // If the JSON object is more complex, you need to increase that value.
-  // See https://bblanchon.github.io/ArduinoJson/assistant/
-  StaticJsonBuffer<512> jsonBuffer;
+  // Inside the brackets, 200 is the RAM allocated to this document.
+  // Don't forget to change this value to match your requirement.
+  // Use arduinojson.org/v6/assistant to compute the capacity.
+  StaticJsonDocument<200> doc;
 
-  // StaticJsonBuffer allocates memory on the stack, it can be
-  // replaced by DynamicJsonBuffer which allocates in the heap.
+  // StaticJsonObject allocates memory on the stack, it can be
+  // replaced by DynamicJsonDocument which allocates in the heap.
   //
-  // DynamicJsonBuffer  jsonBuffer(512);
+  // DynamicJsonDocument  doc(200);
 
-  // Create the root of the object tree.
+  // Add values in the document
   //
-  // It's a reference to the JsonObject, the actual bytes are inside the
-  // JsonBuffer with all the other nodes of the object tree.
-  // Memory is freed when jsonBuffer goes out of scope.
-  JsonObject &root = jsonBuffer.createObject();
+  doc["sensor"] = "gps";
+  doc["time"] = 1351824120;
 
-  // Add values in the object
+  // Add an array.
   //
-  // Most of the time, you can rely on the implicit casts.
-  // In other case, you can do root.set<long>("time", 1351824120);
-  root["sensor"] = "gps";
-  root["time"] = 1351824120;
-
-  // Add a nested array.
-  //
-  // It's also possible to create the array separately and add it to the
-  // JsonObject but it's less efficient.
-  JsonArray &data = root.createNestedArray("data");
+  JsonArray data = doc.createNestedArray("data");
   data.add(48.756080);
   data.add(2.302038);
 
-  std::cout << root << std::endl;
+  std::cout << doc << std::endl;
   // This prints:
   // {"sensor":"gps","time":1351824120,"data":[48.756080,2.302038]}
 
